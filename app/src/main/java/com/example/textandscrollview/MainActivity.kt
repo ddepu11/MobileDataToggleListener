39f76ff5116eeb73bff00e6223f1c8697a77a7a7
deepanshu tiwari <deepanshut@geekyants.com>
2023-10-04 19:26:10 +0530

    use UserMobileDataStateListener to get if data enabled or not

--- app/src/main/java/com/example/textandscrollview/MainActivity.kt
+++ app/src/main/java/com/example/textandscrollview/MainActivity.kt
@@ -1,18 +1,52 @@
 package com.example.textandscrollview
 
+import android.content.Context
+import android.os.Build
 import android.os.Bundle
-import android.widget.TextView
+import android.telephony.PhoneStateListener
+import android.telephony.TelephonyCallback
+import android.telephony.TelephonyDisplayInfo
+import android.telephony.TelephonyManager
+import android.util.Log
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
+import java.util.concurrent.Executors
 
 class MainActivity : AppCompatActivity() {
 
+  @RequiresApi(Build.VERSION_CODES.R)
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
 
-    val textView1 = findViewById<TextView>(R.id.textView1)
+    val tag = "NEW_TAG"
+    val exec = Executors.newSingleThreadExecutor()
 
+    val telephonyManager =
+        application.getSystemService(Context.TELEPHONY_SERVICE) as TelephonyManager
 
+    if (Build.VERSION.SDK_INT >= 31) {
 
+      val callback =
+          object : TelephonyCallback(), TelephonyCallback.UserMobileDataStateListener {
+
+            override fun onUserMobileDataStateChanged(enabled: Boolean) {
+              Log.d(tag, enabled.toString())
+            }
+          }
+
+      telephonyManager.registerTelephonyCallback(exec, callback)
+    } else {
+
+      @Suppress("OVERRIDE_DEPRECATION")
+      val callback =
+          @RequiresApi(Build.VERSION_CODES.Q)
+          object : PhoneStateListener(exec) {
+            override fun onDisplayInfoChanged(telephonyDisplayInfo: TelephonyDisplayInfo) {
+              Log.d(tag, telephonyDisplayInfo.toString())
+            }
+          }
+      telephonyManager.listen(callback, PhoneStateListener.LISTEN_DISPLAY_INFO_CHANGED)
+    }
   }
 }
