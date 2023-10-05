660dff01df055b7e863cc83fc26f253672379c98
deepanshu tiwari <ddepu11@gmail.com>
2023-10-05 13:18:15 +0530

    chore: remove unused code

--- app/src/main/java/com/example/mobiledata/MainActivity.kt
+++ app/src/main/java/com/example/mobiledata/MainActivity.kt
@@ -11,15 +11,19 @@ import android.telephony.TelephonyCallback
 import android.telephony.TelephonyCallback.UserMobileDataStateListener
 import android.telephony.TelephonyManager
 import android.util.Log
-import android.widget.Toast
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.coroutines.DelicateCoroutinesApi
 import java.util.concurrent.Executors
 import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.channels.awaitClose
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.callbackFlow
 import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
 
+  @OptIn(DelicateCoroutinesApi::class)
   @RequiresApi(Build.VERSION_CODES.R)
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
@@ -27,41 +31,58 @@ class MainActivity : AppCompatActivity() {
 
     val tag = "NEW_TAG"
 
-    val telephonyManager =
-        application.getSystemService(Context.TELEPHONY_SERVICE) as TelephonyManager
+    //   #######################_________@@@@@@@@@@@@@@@@@@@@@@@@@@@
+    //   Try with Kotlin flow
 
-    if (Build.VERSION.SDK_INT >= 31) {
+    GlobalScope.launch { getMobileDataToggleUpdatesFlow().collect { Log.d(tag, "MDATA: $it") } }
+  }
 
-      GlobalScope.launch {
-        val exec = Executors.newCachedThreadPool()
+  private fun getMobileDataToggleUpdatesFlow(): Flow<Any> {
+    return callbackFlow<Any> {
+      val tag = "NEW_TAG"
+
+      val telephonyManager =
+          application.getSystemService(Context.TELEPHONY_SERVICE) as TelephonyManager
+      val exec = Executors.newCachedThreadPool()
+
+      if (Build.VERSION.SDK_INT >= 31) {
 
         val callback =
             object : TelephonyCallback(), UserMobileDataStateListener {
 
               override fun onUserMobileDataStateChanged(enabled: Boolean) {
-                Log.d(tag, "MData: ${enabled.toString()}")
+                trySend(enabled)
               }
             }
 
         telephonyManager.registerTelephonyCallback(exec, callback)
-      }
-    } else {
-      Log.d(tag, "State: UNDER SDK 30")
 
-      val mObserver: ContentObserver =
-          object : ContentObserver(Handler()) {
-            override fun onChange(selfChange: Boolean, uri: Uri?) {
-              // Retrieve mobile data value here and perform necessary actions
-              Log.d(tag, "MData: $selfChange")
-              Toast.makeText(applicationContext, "MData: $selfChange", Toast.LENGTH_LONG).show()
+        awaitClose {
+          telephonyManager.unregisterTelephonyCallback(callback)
+          exec.shutdown()
+          Log.d(tag, "CLOSE!!!")
+        }
+      } else {
+        Log.d(tag, "State: UNDER SDK 30")
+
+        val mObserver: ContentObserver =
+            object : ContentObserver(Handler()) {
+              override fun onChange(selfChange: Boolean, uri: Uri?) {
+                trySend(selfChange)
+              }
             }
-          }
 
-      val mobileDataSettingUri: Uri = Settings.Secure.getUriFor("mobile_data")
+        val mobileDataSettingUri: Uri = Settings.Secure.getUriFor("mobile_data")
 
-      getApplicationContext()
-          .getContentResolver()
-          .registerContentObserver(mobileDataSettingUri, true, mObserver)
+        getApplicationContext()
+            .getContentResolver()
+            .registerContentObserver(mobileDataSettingUri, true, mObserver)
+
+        awaitClose {
+          getContentResolver().unregisterContentObserver(mObserver)
+          exec.shutdown()
+        }
+      }
     }
   }
 }
